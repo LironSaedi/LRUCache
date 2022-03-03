1412394374e834f72ab080f0c7720b85f224895b
Liron Saedi <Liron.Saedi@GMR.local>
2022-03-02 18:17:40 -0800

    Initial COmmiet

---
+++ KeyValuePair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LRUCache
+{
+    class KeyValuePair<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        public KeyValuePair(TKey key, TValue val)
+        {
+            Key = key;
+            Value = val;
+        }
+    }
+}
---
+++ LRUCache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LRUCache
+{
+    public interface ICache<TKey, TValue>
+    {
+        bool TryGetValue(TKey key, out TValue value);
+        void Put(TKey key, TValue value);
+    }
+    class LRUCache<Tkey, TValue> : ICache<Tkey, TValue>
+    {
+
+    }
+}
+
